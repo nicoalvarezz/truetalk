56c76d71152b8178929539126798d4373c7d57e4
Nicolas <nicoalavarezgarrido@gmail.com>
2022-09-29 21:06:59 +0100

    project setup

---
+++ src/main/java/com/fyp/alethia/AlethiaApplication.java
@@ -0,0 +1,13 @@
+package com.fyp.alethia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlethiaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlethiaApplication.class, args);
+	}
+
+}
