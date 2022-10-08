de2844f88f91d385d8aa36de2abb8ac37ae42623
Nicolas <nicoalavarezgarrido@gmail.com>
2022-10-08 22:39:37 +0100

    user-service set up

---
+++ user-service/src/test/java/com/fyp/alethia/UserServiceApplicationTest.java
@@ -0,0 +1,13 @@
+package com.fyp.alethia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserServiceApplicationTest {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
