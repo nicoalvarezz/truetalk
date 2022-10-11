18fc487bf86390258e76346a5e09649ebf68deb1
Nicolas <nicoalavarezgarrido@gmail.com>
2022-10-11 14:49:14 +0100

    FYP-6 doing some clean up, only return response left -> Renew access token outside the scope of story

--- alethia-service/src/main/java/com/fyp/alethiaservice/service/AlethiaService.java
+++ alethia-service/src/main/java/com/fyp/alethiaservice/service/AlethiaService.java
@@ -5,6 +5,7 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fyp.alethiaservice.dto.IDPalRequest;
 import com.fyp.alethiaservice.dto.UserRequest;
+import lombok.SneakyThrows;
 import okhttp3.MediaType;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
@@ -24,6 +25,9 @@ import java.util.Map;
 @PropertySource("classpath:application.properties")
 public class AlethiaService {
 
+    // There is a better way on how to do this -> Remember that this variables are not static,
+    // so it should not be written in Upper Case
+    // Use something that is global to the class (I don't remember the technical name, but that exists)
     @Value("${idpal.apiAccess.clientKey}")
     private String IDPAL_CLIENT_KEY;
 
@@ -50,12 +54,12 @@ public class AlethiaService {
 
     private static String INFORMATION_TYPE = "email";
     private static ObjectMapper MAPPER = new ObjectMapper().setSerializationInclusion(JsonInclude.Include.NON_NULL);
-    private final OkHttpClient httpClient = new OkHttpClient();
     private static final MediaType JSON = MediaType.parse("application/json; charset=utf-8");
+
+    private final OkHttpClient httpClient = new OkHttpClient();
 
     // TODO:
     // Understand how ID-Pal access token work, how should it be stored? Does it expire? If so, how to renew access token?
-    // Better understand SneakyThrows, how does it work? How does it affect performance?
     // What should happen with the response? 200 response right? -> That is what makes sense to me
 
     public void triggerVerification(UserRequest registerUserData) throws JsonProcessingException {
@@ -67,38 +71,15 @@ public class AlethiaService {
                 .profileId(IDPAL_PROFILE_ID)
                 .build();
 
-//        System.out.println(CLIENT_KEY);
-//        System.out.println(IDPAL_SEND_ENDPOINT);
-//        System.out.println(PROFILE_ID);
-
         RequestBody body = RequestBody.create(MAPPER.writeValueAsString(idPalRequest), JSON);
-        Request request = new Request.Builder()
-                .url(IDPAL_SEND_ENDPOINT)
-                // I need to investigate how access token work and how would be the best way to store them
-                .addHeader("Authorization", "Bearer " + IDPAL_ACCESS_TOKEN)
-                .addHeader("Accept", "application/json")
-                .post(body)
-                .build();
+        Response response = makeIdPalRequest(
+                generateIdPalRequest(IDPAL_SEND_ENDPOINT, body)
+        );
 
-        getAccessToken();
-//        do {
-//            try {
-//                Response response = httpClient.newCall(request).execute();
-////                System.out.println(response.body().string());
-//                Map<String, String> responseMap = MAPPER.readValue(response.body().toString(), Map.class);
-//                if (responseMap.containsKey("error")) {
-//                    getAccessToken();
-//                }
-//
-//            } catch (IOException e) {
-//                // What status code should this return -> I should never return a 5xx
-//                // This will happen due to cancellation, a connectivity problem or a timeout
-//                e.printStackTrace();
-//            }
-//        } while( access_token is expired is the condition);
+        // What does this return ?????
     }
 
-    private void getAccessToken() throws JsonProcessingException {
+    private void renewAccessToken() throws JsonProcessingException {
         IDPalRequest idPalRequest = IDPalRequest.builder()
                 .clientKey(IDPAL_CLIENT_KEY)
                 .accessKey(IDPAL_ACCESS_KEY)
@@ -107,25 +88,22 @@ public class AlethiaService {
                 .build();
 
         RequestBody body = RequestBody.create(MAPPER.writeValueAsString(idPalRequest), JSON);
-        System.out.println(MAPPER.writeValueAsString(idPalRequest));
+        Response response = makeIdPalRequest(
+                generateIdPalRequest(IDPAL_GET_ACCESS_TOKEN_ENDPOINT, body)
+        );
+    }
 
-        Request request = new Request.Builder()
-                .url(IDPAL_GET_ACCESS_TOKEN_ENDPOINT)
-                // I need to investigate how access token work and how would be the best way to store them
-                .addHeader("Authorization", "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsImp0aSI6IjA3OTliY2JhMDViZjBiMmM0MmJiMjY2ZDVkODhmODdmMTEwZWM0MTYxZjIxMTVhYjQ3NzMxOTUxNjMwYjA1NjZiMWRkMmFjODYzNGJjYTVkIn0") // Do not forget about Bearer
+    private Request generateIdPalRequest(String endpoint, RequestBody body) {
+        return new Request.Builder()
+                .url(endpoint)
+                .addHeader("Authorization", "Bearer " + IDPAL_ACCESS_TOKEN)
                 .addHeader("Accept", "application/json")
                 .post(body)
                 .build();
+    }
 
-        try {
-            Response response = httpClient.newCall(request).execute();
-            Map<String, String> responseMap = MAPPER.readValue(response.body().string(), Map.class);
-            IDPAL_ACCESS_TOKEN = responseMap.get("access_token");
-            IDPAL_REFRESH_TOKEN = responseMap.get("refresh_token");
-        } catch (IOException e) {
-            // What status code should this return -> I should never return a 5xx
-            // This will happen due to cancellation, a connectivity problem or a timeout
-            e.printStackTrace();
-        }
+    @SneakyThrows
+    private Response makeIdPalRequest(Request request) {
+        return httpClient.newCall(request).execute();
     }
 }
