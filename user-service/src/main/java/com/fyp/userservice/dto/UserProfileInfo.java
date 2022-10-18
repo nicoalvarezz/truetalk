5884732a38187a37f5e461235f99a303fde12669
Nicolas <nicoalavarezgarrido@gmail.com>
2022-10-18 14:59:52 +0100

    FYP-23 setting up user profile receiver endpoint [user-service]

---
+++ user-service/src/main/java/com/fyp/userservice/dto/UserProfileInfo.java
@@ -0,0 +1,53 @@
+package com.fyp.userservice.dto;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserProfileInfo {
+    @JsonProperty("firstname")
+    private String firstName;
+
+    @JsonProperty("lastname")
+    private String lastName;
+
+    private String email;
+
+    @JsonProperty("account_id")
+    private String accountId;
+
+    @JsonProperty("phone_country_code")
+    private String phoneCountryCode;
+
+    @JsonProperty("phone")
+    private String phoneNumber;
+
+    private String gender;
+
+    @JsonProperty("dob")
+    private String dateOfBirth;
+
+    @JsonProperty("countryofbirth")
+    private String countryOfBirth;
+
+    private String address1;
+
+    private String address2;
+
+    private String city;
+
+    private String county;
+
+    @JsonProperty("country_name")
+    private String countryName;
+
+    @JsonProperty("postalcode")
+    private String postalCode;
+}
