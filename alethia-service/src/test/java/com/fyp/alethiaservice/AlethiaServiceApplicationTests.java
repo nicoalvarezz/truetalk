4ef0defd705b13b7c332a81c910c946b3f992209
Nicolas <nicoalavarezgarrido@gmail.com>
2022-11-13 22:55:38 +0000

    FYP-43 firs test [alethia-service]

--- alethia-service/src/test/java/com/fyp/alethiaservice/AlethiaServiceApplicationTests.java
+++ alethia-service/src/test/java/com/fyp/alethiaservice/AlethiaServiceApplicationTests.java
@@ -1,13 +1,42 @@
 package com.fyp.alethiaservice;
 
+import com.fyp.alethiaservice.dto.users.UserRequest;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.testcontainers.shaded.com.fasterxml.jackson.databind.ObjectMapper;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
+@AutoConfigureMockMvc
 class AlethiaServiceApplicationTests {
 
+	@Autowired
+	private MockMvc mockMvc;
+
+	private ObjectMapper mapper = new ObjectMapper();
+
+	private static final String VALID_EMAIL = "nicoalvarezgarrido@gmail.com";
+	private static final String VALID_PHONE_NUMBER = "+353123456789";
+
 	@Test
-	void contextLoads() {
+	void testTriggerVerificationEndpoint() throws Exception {
+		mockMvc.perform(MockMvcRequestBuilders.post("/api/alethia/trigger-verification")
+					.contentType(MediaType.APPLICATION_JSON)
+					.content(mapper.writeValueAsString(
+							getUserRequest(VALID_EMAIL, VALID_PHONE_NUMBER))))
+				.andExpect(status().isOk());
 	}
 
+	private UserRequest getUserRequest(String email, String phoneNumber) {
+		return UserRequest.builder()
+				.email(email)
+				.phoneNumber(phoneNumber)
+				.build();
+	}
 }
