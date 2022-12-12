1f7bfc84ba48bac495830a0dd13db8cc218979a4
Nicolas <nicoalavarezgarrido@gmail.com>
2022-12-12 21:05:18 +0000

    FYP-62 code reutilsation ApiHelpers [alethoia-service]

--- hive-shared/src/main/java/com/fyp/hiveshared/api/helpers/ApiHelpers.java
+++ hive-shared/src/main/java/com/fyp/hiveshared/api/helpers/ApiHelpers.java
@@ -1,4 +1,4 @@
-package com.fyp.alethiaservice.service;
+package com.fyp.hiveshared.api.helpers;
 
 import okhttp3.Headers;
 import okhttp3.OkHttpClient;
@@ -12,19 +12,18 @@ import org.springframework.http.HttpStatus;
 import java.io.IOException;
 
 public class ApiHelpers {
-
     private static final Logger LOGGER = LoggerFactory.getLogger(ApiHelpers.class);
-    private static final String REQUEST_EXCEPTION = "Something occurred down the service that has been called";
     private static final OkHttpClient HTTP_CLIENT = new OkHttpClient();
+    private static final String REQUEST_EXCEPTION = "Something occurred down the service that has been called";
 
-    public static Response makeAPIRequest(Request request) {
+    public static Response makeApiRequest(Request request) {
         try {
             return HTTP_CLIENT.newCall(request).execute();
         } catch (IOException e) {
             LOGGER.error(e.toString());
             return new Response.Builder()
                     .message(REQUEST_EXCEPTION)
-                    .code(HttpStatus.OK.value())
+                    .code(HttpStatus.SERVICE_UNAVAILABLE.value())
                     .build();
         }
     }
@@ -39,30 +38,10 @@ public class ApiHelpers {
                                 .add("Accept", "application/json")
                                 .build();
 
-        Request request = new Request.Builder()
+        return new Request.Builder()
                 .url(endpoint)
                 .headers(headers)
                 .method(method, body)
                 .build();
-
-        return request;
     }
-
-//    private void renewIdPalAccessToken() throws JsonProcessingException {
-//        IDPalRequest idPalRequest = IDPalRequest.builder()
-//                .clientKey(idpalClientKey)
-//                .accessKey(idpalAccessKey)
-//                .clientId(idpalClientId)
-//                .refreshToken(idpalRefreshToken)
-//                .build();
-//
-//        Response responseMap = makeAPIRequest(
-//                generateRequest(
-//                        POST_METHOD,
-//                        idpalGetAccessTokenEndpoint,
-//                        RequestBody.create(MAPPER.writeValueAsString(idPalRequest), JSON),
-//                        idpalAccessToken
-//                )
-//        );
-//    }
 }
