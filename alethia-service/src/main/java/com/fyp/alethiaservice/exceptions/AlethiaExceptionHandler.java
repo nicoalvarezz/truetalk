e0436fd6791e4352f1f829bd86fde91f7ef9b52d
Nicolas <nicoalavarezgarrido@gmail.com>
2022-12-18 09:59:08 +0100

    FYP-68 refactoring exceptin handler responses

--- alethia-service/src/main/java/com/fyp/alethiaservice/exceptions/AlethiaExceptionHandler.java
+++ alethia-service/src/main/java/com/fyp/alethiaservice/exceptions/AlethiaExceptionHandler.java
@@ -1,6 +1,5 @@
 package com.fyp.alethiaservice.exceptions;
 
-import com.fyp.hiveshared.api.responses.HiveResponseBody;
 import com.fyp.hiveshared.api.responses.excpetion.ServiceUnavailableException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -9,35 +8,22 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 
-import java.time.ZoneId;
-import java.time.ZonedDateTime;
+import static com.fyp.hiveshared.api.responses.excpetion.ExceptionHandlerResponses.exceptionHandlerResponse;
 
 
 @ControllerAdvice
 public class AlethiaExceptionHandler {
 
+    private static final String SERVICE = "alethia-service";
     private static final String METHOD_ARGUMENT_ERROR = "Validation failed for some of the arguments. Make sure that all arguments are correct";
-    private static final String ALETHIA_SERVICE = "alethia-service";
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
-    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex, WebRequest request) {
-        return new ResponseEntity<>(new HiveResponseBody(
-                METHOD_ARGUMENT_ERROR,
-                HttpStatus.BAD_REQUEST.value(),
-                HttpStatus.BAD_REQUEST,
-                ALETHIA_SERVICE,
-                ZonedDateTime.now(ZoneId.of("Z")).toString()
-        ), HttpStatus.BAD_REQUEST);
+    public ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex, WebRequest request) {
+        return exceptionHandlerResponse(METHOD_ARGUMENT_ERROR, HttpStatus.BAD_REQUEST, SERVICE);
     }
 
     @ExceptionHandler(ServiceUnavailableException.class)
     public ResponseEntity<?> handleServiceUnavailableException(ServiceUnavailableException ex, WebRequest request) {
-        return new ResponseEntity<>(new HiveResponseBody(
-                ex.getMessage(),
-                HttpStatus.SERVICE_UNAVAILABLE.value(),
-                HttpStatus.SERVICE_UNAVAILABLE,
-                ALETHIA_SERVICE,
-                ZonedDateTime.now(ZoneId.of("Z")).toString()
-        ), HttpStatus.SERVICE_UNAVAILABLE);
+        return exceptionHandlerResponse(ex.getMessage(), HttpStatus.SERVICE_UNAVAILABLE, SERVICE);
     }
 }
--- user-service/src/main/java/com/fyp/userservice/exception/UserExceptionHandler.java
+++ user-service/src/main/java/com/fyp/userservice/exception/UserExceptionHandler.java
@@ -1,6 +1,5 @@
 package com.fyp.userservice.exception;
 
-import com.fyp.hiveshared.api.responses.HiveResponseBody;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
@@ -8,23 +7,16 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 
-import java.time.ZoneId;
-import java.time.ZonedDateTime;
+import static com.fyp.hiveshared.api.responses.excpetion.ExceptionHandlerResponses.exceptionHandlerResponse;
 
 @ControllerAdvice
 public class UserExceptionHandler {
 
-    private static final String USER_SERVICE = "user-service";
+    private static final String SERVICE = "user-service";
     private static final String METHOD_ARGUMENT_ERROR = "Validation failed for some of the arguments. Make sure that all arguments are correct";
 
     @ExceptionHandler({MethodArgumentNotValidException.class})
     protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex, WebRequest request) {
-        return new ResponseEntity<>(new HiveResponseBody(
-                METHOD_ARGUMENT_ERROR,
-                HttpStatus.BAD_REQUEST.value(),
-                HttpStatus.BAD_REQUEST,
-                USER_SERVICE,
-                ZonedDateTime.now(ZoneId.of("Z")).toString()
-        ), HttpStatus.BAD_REQUEST);
+        return exceptionHandlerResponse(METHOD_ARGUMENT_ERROR, HttpStatus.BAD_REQUEST, SERVICE);
     }
 }
