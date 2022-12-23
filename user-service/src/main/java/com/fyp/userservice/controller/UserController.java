1a7bbe94886a7263ed874f7623496b8dd3dae1ea
Nicolas <nicoalavarezgarrido@gmail.com>
2022-12-23 01:37:55 +0100

    FYP-74 quick clean alethia test, new register endpoint, some clarifying comments

--- user-service/src/main/java/com/fyp/userservice/controller/UserController.java
+++ user-service/src/main/java/com/fyp/userservice/controller/UserController.java
@@ -24,6 +24,19 @@ public class UserController {
     private final UserService userService;
     private static final String SERVICE = "user-service";
 
+    @PostMapping("/register")
+    @ResponseBody
+    public ResponseEntity<Object> registerUser(@Valid RegisterUserRequest registerUserRequest) {
+        // TODO:
+        // simply save the data in the database...
+        // email, password in the user table
+        // The the email verification will start
+        return ResponseHandler.serviceResponse("User registered successfully", HttpStatus.CREATED, SERVICE);
+    }
+
+    // TODO:
+    // Define when this endpoint will be called ?????
+    // This is something that has me confused as when this endpoint will be triggered...
     @PostMapping("/trigger-alethia-verification")
     @ResponseBody
     public ResponseEntity<Object> triggerAlethiaVerification(@Valid @RequestBody RegisterUserRequest registerUserRequest) throws IOException {
