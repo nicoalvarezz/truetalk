622b283761296a23b91b50f3b5ccc0bc7a6e9ea7
Nicolas <nicoalavarezgarrido@gmail.com>
2023-01-29 22:57:50 +0000

    FYP-98 processing and saving user post

---
+++ post-service/src/main/java/com/fyp/postservice/service/PostService.java
@@ -0,0 +1,36 @@
+package com.fyp.postservice.service;
+
+import com.fyp.postservice.dto.UserPost;
+import com.fyp.postservice.model.Post;
+import com.fyp.postservice.repository.PostRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+
+import java.time.Instant;
+import java.util.UUID;
+
+
+@Service
+@Component
+@RequiredArgsConstructor
+public class PostService {
+
+    private final PostRepository postRepository;
+
+    public void processAndSavePost(UserPost userPost) {
+        Post post = Post.builder()
+                .cratedAt(String.valueOf(Instant.now().getEpochSecond()))
+                .text(verifyUuid(userPost.getText()))
+                .user(userPost.getUser())
+                .likes(0)
+                .build();
+
+        postRepository.save(post);
+    }
+
+    private String verifyUuid(String uuid) {
+        UUID.fromString(uuid);
+        return uuid;
+    }
+}
