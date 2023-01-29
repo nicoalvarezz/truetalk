2f0384d964c0a5fe83ed395efc48d9dcb78a81fd
Nicolas <nicoalavarezgarrido@gmail.com>
2023-01-29 19:55:23 +0000

    FYP-97 removing verification code

--- post-service/src/main/java/com/fyp/postservice/controller/PostsController.java
+++ post-service/src/main/java/com/fyp/postservice/controller/PostsController.java
@@ -21,14 +21,6 @@ public class PostsController {
 
     @GetMapping("/test")
     public ResponseEntity<Object> test() {
-        Post post = Post.builder()
-                .cratedAt("Wed Sep 09 00:00:00 +0000 2020")
-                .text("Testing that the post is saved in the db")
-                .likes(12)
-                .user("682c8bae-9f43-11ed-9be3-4395a0af5ff3")
-                .build();
-
-        postRepository.save(post);
         return ResponseHandler.responseBody("Hello from post-service", HttpStatus.OK, SERVICE);
     }
 }
