10900e245ac51ad9f79d8222255a3a088ae65826
Nicolas <nicoalavarezgarrido@gmail.com>
2023-02-02 15:43:04 +0000

    FYP-107 cleaning up UserServiceTests

--- user-service/src/test/java/com/fyp/userservice/UserServiceTests.java
+++ user-service/src/test/java/com/fyp/userservice/UserServiceTests.java
@@ -19,37 +19,12 @@ import org.testcontainers.junit.jupiter.Testcontainers;
 @Testcontainers
 public class UserServiceTests {
 
-
-    @Container
-    static PostgreSQLContainer postgreSQLContainer = new PostgreSQLContainer("postgres:latest");
-
-    @Autowired
-    private UserRepository userRepository;
-
-    @Autowired
-    private UserVerifiedProfileRepository userVerifiedProfileRepository;
-
     @Autowired
     private UserService userService;
-
-    private static final ObjectMapper MAPPER = new ObjectMapper();
 
     private static final String RECEIVE_USER_PROFILE = "/api/users/receive-user-profile";
     private static final String VALID_EMAIL = "nicoalvarezgarrido@gmail.com";
     private static final String VALID_PHONE_NUMBER = "+3530838455233";
     private static final String VALID_PASSWORD = "dcfnsjdvffnA9cdnsj@";
-
-    private static final String SERVICE = "user-service";
-    private static final String TRIGGER_ALETHIA_VERIFICATION_ENDPOINT = "/api/users/trigger-alethia-verification";
-
-    @DynamicPropertySource
-    static void setProperties(DynamicPropertyRegistry dynamicPropertyRegistry) {
-        dynamicPropertyRegistry.add("spring.data.postgres.uri", postgreSQLContainer::getJdbcUrl);
-    }
-
-    @Test
-    void testCreateUserValidInput() {
-
-    }
 
 }
