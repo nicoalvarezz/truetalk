61e298c227347a641f359a0cd278a361e3af7670
Nicolas <nicoalavarezgarrido@gmail.com>
2023-02-02 15:43:58 +0000

    FYP-107 more clean up

--- user-service/src/test/java/com/fyp/userservice/UserServiceTests.java
+++ user-service/src/test/java/com/fyp/userservice/UserServiceTests.java
@@ -1,17 +1,9 @@
 package com.fyp.userservice;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fyp.userservice.repository.UserRepository;
-import com.fyp.userservice.repository.UserVerifiedProfileRepository;
 import com.fyp.userservice.service.UserService;
-import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.DynamicPropertyRegistry;
-import org.springframework.test.context.DynamicPropertySource;
-import org.testcontainers.containers.PostgreSQLContainer;
-import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 
 @SpringBootTest
