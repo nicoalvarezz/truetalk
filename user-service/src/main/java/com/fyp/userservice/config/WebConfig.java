5fab5b41aa2c78f66258872a68e56cfb8cad84d6
Nicolas <nicoalavarezgarrido@gmail.com>
2023-03-18 19:52:28 +0000

    FYP-143 cors again

--- user-service/src/main/java/com/fyp/userservice/config/WebConfig.java
+++ user-service/src/main/java/com/fyp/userservice/config/WebConfig.java
@@ -13,12 +13,13 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         CorsRegistration corsRegistration = registry.addMapping("/**");
-        corsRegistration.allowedOrigins("http://localhost:3000");
-        corsRegistration.allowedOrigins("http://192.168.0.161:3000");
-        corsRegistration.allowedOrigins("http://143.42.26.143");
-        corsRegistration.allowedOrigins("http://143.42.26.143:80");
-        corsRegistration.allowedOrigins("http://172.105.75.93:8080");
-        corsRegistration.allowedOrigins("http://192.46.239.71:80");
+//        corsRegistration.allowedOrigins("http://localhost:3000");
+//        corsRegistration.allowedOrigins("http://192.168.0.161:3000");
+//        corsRegistration.allowedOrigins("http://143.42.26.143");
+//        corsRegistration.allowedOrigins("http://143.42.26.143:80");
+//        corsRegistration.allowedOrigins("http://172.105.75.93:8080");
+//        corsRegistration.allowedOrigins("http://192.46.239.71:80");
+        corsRegistration.allowedOrigins("*");
         corsRegistration.allowedMethods("GET", "POST", "PUT", "DELETE");
     }
 }
